2d982727c8be647cf795a582eee78f493f9db44b
Dhiraj <dhiraj.basavaraju@gmail.com>
2024-11-19 03:22:27 +0530

    ++

---
+++ 2. Indentation.py
@@ -0,0 +1,17 @@
+# indentation is mandatory & important
+
+x = 5
+y = "Number is even"
+z = "Number is odd"
+
+if x%2==0 :
+    print(str(x)+" "+y)
+    print(x,y)
+else :
+    print(str(x)+" "+z)
+    print(x,y)
+"""
+ps
+this is multi line
+comment
+"""
---
+++ 3. GlobalVariable.py
@@ -0,0 +1,12 @@
+a = "Python is"
+b = "awesome"
+
+def func_one() :
+    print(a,b)
+
+def func_two() :
+    b = "really good"
+    print(a,b)
+
+func_one()
+func_two()
