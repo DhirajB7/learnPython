4d77b3f86c8efec744c8c9a877b5e9cac85fe401
Dhiraj <dhiraj.basavaraju@gmail.com>
2024-11-20 02:39:03 +0530

    Create 7. String.py

---
+++ 7. String.py
@@ -0,0 +1,39 @@
+a = "Hello"
+b = 'Hello world'
+c = "my name is 'DHIRAJ'..."
+d = """
+i 
+am 
+multi 
+line 
+"""
+print(a)
+print(b)
+print(c)
+print(d)
+print("=====================")
+print(a,b,c,d)
+print("=====================")
+name = "Dhiraj"
+print(len(name))
+print(name[0],name[-1])
+print("=====================")
+for x in name :
+    print(x)
+print("=====================")
+txt = "The best things in life are free!"
+print("free" in txt)
+print("free" not in txt)
+print("=====================")
+print(b[2:7])
+print("=====================")
+slice_example="abcdefghijklmnopqrstuvwxyz"
+print(slice_example[2:7]) ##cdefg
+print(slice_example[7:2]) ##EMPTY
+print(slice_example[:7]) ##abcdefg
+print(slice_example[7:]) ##hijklmnopqrstuvwxyz
+print(slice_example[-5:-2])##vwx
+print(slice_example[-2:-5])## EMPTY
+print(slice_example[-5:]) ##vwxyz
+print(slice_example[:-2]) ##abcdefghijklmnopqrstuv
+
